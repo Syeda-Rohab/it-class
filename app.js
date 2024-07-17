63423e1f47fbc145d1e64d3682a579f56fc7636d
Syeda-Rohab <syedarohab8@gmail.com>
2024-07-17 00:59:25 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+// console.log = jab likhte hain jab hamen terminal par wo word print karwana ho ;
+// symbol = console.log ( " Thank you kamran khan tessori for giving us this oppurtunity " ) ;
+console.log("hello world");
---
+++ app.ts
@@ -0,0 +1,9 @@
+// console.log = jab likhte hain jab hamen terminal par wo word print karwana ho ;
+
+
+
+// symbol = console.log ( " Thank you kamran khan tessori for giving us this oppurtunity " ) ;
+
+
+
+console.log ( "hello world" ) ; 
